fa7fda56b61b3cc8afd3e5211b52269eecc7f595
Grzegorz <grzegorz.wilczynski@student.put.poznan.pl>
2018-10-20 17:47:27 +0200

    Histogram + wyświetlanie wyniku

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 import numpy as np
 
 
@@ -11,6 +10,7 @@ PROBABILITY_OF_BEING_IN_HOTEL = 0.1     # parameter P
 def main():
     suspicious_pairs = 0
     suspicious_days_pairs = 0
+    histogram = dict()
 
     meeting_counter = np.zeros((AMOUNT_OF_PEOPLE, AMOUNT_OF_PEOPLE))
     hotels_guests = [[] for i in range(AMOUNT_OF_HOTELS)]
@@ -30,10 +30,17 @@ def main():
         for second_person in range(person + 1, AMOUNT_OF_PEOPLE):
             if meeting_counter[person][second_person] > 1:
                 suspicious_pairs += 1
-                suspicious_days_pairs += (meeting_counter[person][second_person]
-                                          * (meeting_counter[person][second_person] - 1)) / 2
+                suspicious_days_pairs += int((meeting_counter[person][second_person]
+                                          * (meeting_counter[person][second_person] - 1)) / 2)
+            if meeting_counter[person][second_person] > 0:
+                if meeting_counter[person][second_person] in histogram:
+                    histogram[meeting_counter[person][second_person]] += 1
+                else:
+                    histogram[meeting_counter[person][second_person]] = 1
 
-    print(suspicious_pairs, suspicious_days_pairs)
+    print("Podejrzane pary \"osób i dni\": ",  suspicious_days_pairs)
+    print("Podejrzane pary: ", suspicious_pairs)
+    print("Histogram: ", histogram)
 
 
 main()
